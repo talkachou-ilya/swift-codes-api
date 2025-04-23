3fdaca5b2c0df8fa2e52f00bb1fee2e0e41f11d9
Ilya Talkachou <talkachou.ilya@gmail.com>
2025-04-23 23:16:09 +0200

    Refactor test cases and update Dockerfile collection name
    Refactored the test cases for `GetSwiftCode` and `GetSwiftCodesByCountry` to improve clarity and modularity. Adjusted the collection name in the seed `Dockerfile` to align with naming conventions (`swift-codes`). This enhances test organization and ensures consistency in configuration.

--- tests/unit/get_swift_code_test.go
+++ tests/unit/get_swift_code_test.go
@@ -1,38 +1,48 @@
 package unit
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"swift-codes-api/tests/unit/test_cases"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"swift-codes-api/handlers"
 	"swift-codes-api/internal/config"
 	mockRepos "swift-codes-api/repositories/mock"
-	"swift-codes-api/tests/unit/test_cases"
-	"testing"
 )
 
-func TestGetSwiftCodesByCountry(t *testing.T) {
+func TestGetSwiftCode(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
-	for _, tc := range test_cases.GetCountryTestCases() {
+	for _, tc := range test_cases.GetSwiftCodeTestCases() {
 		t.Run(tc.Name, func(t *testing.T) {
 			mockRepo := new(mockRepos.SwiftRepository)
 			tc.SetupMocks(mockRepo)
 
 			handler := handlers.NewSwiftHandler(config.Config{}, mockRepo)
+			router := setupRouter(handler)
 
-			router := gin.Default()
-			router.GET("/swift-codes/country/:countryISO2code", handler.GetSwiftCodesByCountry)
+			response := performRequest(router, tc.SwiftCode)
 
-			recorder := httptest.NewRecorder()
-			req, _ := http.NewRequest(http.MethodGet, "/swift-codes/country/"+tc.CountryISO2, nil)
-			router.ServeHTTP(recorder, req)
+			assert.Equal(t, tc.ExpectedStatusCode, response.Code)
+			assert.JSONEq(t, tc.ExpectedResponse, response.Body.String())
 
-			respBody := recorder.Body.String()
-			require.Equal(t, tc.ExpectedStatus, recorder.Code)
-			require.JSONEq(t, tc.ExpectedResponse, respBody)
 			mockRepo.AssertExpectations(t)
 		})
 	}
+}
+
+func setupRouter(handler *handlers.SwiftCodesHandler) *gin.Engine {
+	router := gin.Default()
+	router.GET("/swift/:swift-code", handler.GetSwiftCode)
+	return router
+}
+
+func performRequest(router *gin.Engine, swiftCode string) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(http.MethodGet, "/swift/"+swiftCode, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	return w
 }
